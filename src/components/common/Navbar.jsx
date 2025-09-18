daab3b1adb5a34a1c86cf9a32a48e41a19ba39a9
Roht <rohittondak38@gmail.com>
2025-09-18 17:33:46 +0530

    New Commit

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -81,9 +81,9 @@ const Navbar = () => {
                                 link.title === "Catalog" ? (<div className=' relative flex items-center group cursor-pointer gap-[5px]'>
                                     <p>{link.title}</p> <IoIosArrowDown className='text-xl font-mono'/>
 
-                                    <div className=' invisible flex flex-col py-4 px-3 text-black bg-richblack-5 rounded-md transition-all duration-200 group-hover:visible  opacity-0 cursor-pointer absolute group-hover:opacity-100 lg:w-[300px] top-10 -left-[104px]  font-medium  gap-2 uppercase'>
+                                    <div className=' invisible flex flex-col py-5 px-3 text-black bg-richblack-5 rounded-md transition-all duration-200 group-hover:visible  opacity-0 cursor-pointer absolute group-hover:opacity-100 lg:w-[300px] top-10 -left-[104px]  font-medium  gap-2 uppercase'>
 
-                                        <div className='bg-richblack-5 absolute w-7 h-7 rotate-45 lg:translate-x-36 lg:translate-y-[-28px] rounded-md'>
+                                        <div className='bg-richblack-5 absolute w-10 h-7 rotate-45 lg:translate-x-[146px] lg:translate-y-[-27px] rounded-md'>
 
                                         </div>
 
